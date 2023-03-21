79c9b0ea25d3b84db61ab5e5ec4a3b758247a4a0
balaji995 <53910685+balaji995@users.noreply.github.com>
2023-03-21 10:29:36 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Components/searchApplication';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
